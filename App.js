493021bc2baa8e3b4a1e9aa41409296df93b01f9
Luiz Gabriel <luizgabriel13112001@gmail.com>
2022-12-07 20:00:31 -0300

    First

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button, Input, Avatar } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <view>
+      <Avatar
+  size="xlarge"
+  source={{
+    uri:
+      'https://cdn-icons-png.flaticon.com/512/3135/3135715.png',
+  }}
+/>
+      </view>
+      <view>
+<Input placeholder='Login' />
+<Input placeholder="Senha" secureTextEntry={true} />
+      </view>
+      <view>
+<Button title="Login"
+buttonStyle={{width: 100, margin:10}}/>
+      </view>
+      <view>
+<Button title="Cadastrar"
+buttonStyle={{backgroundColor: '#F44336', width: 100}}/>
+      </view>
     </View>
   );
 }
@@ -13,7 +35,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#E7DDD7',
     alignItems: 'center',
     justifyContent: 'center',
   },
